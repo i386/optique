e8ca0043ecf8fa3c06e86764f288140fa9f0e937
James Dumay <james.w.dumay@gmail.com>
2013-04-05 18:51:33 +1100

    Use IKImageView subclass instead of NSImageView

---
+++ Optique/OPImageView.h
@@ -0,0 +1,13 @@
+//
+//  OPImageView.h
+//  Optique
+//
+//  Created by James Dumay on 5/04/13.
+//  Copyright (c) 2013 James Dumay. All rights reserved.
+//
+
+#import <ImageKit/ImageKit.h>
+
+@interface OPImageView : IKImageView
+
+@end
---
+++ Optique/OPImageView.m
@@ -0,0 +1,28 @@
+//
+//  OPImageView.m
+//  Optique
+//
+//  Created by James Dumay on 5/04/13.
+//  Copyright (c) 2013 James Dumay. All rights reserved.
+//
+
+#import "OPImageView.h"
+
+@implementation OPImageView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
