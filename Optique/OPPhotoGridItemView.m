9fe0137b04525065971352dc196652ea1ad6e67c
James Dumay <james.w.dumay@gmail.com>
2013-04-18 18:51:53 +1000

    draw selection focus ring

--- Optique/OPPhotoGridItemView.m
+++ Optique/OPPhotoGridItemView.m
@@ -10,6 +10,7 @@
 #import <CNGridView/CNGridViewItemLayout.h>
 #import "OPPhotoGridItemView.h"
 #import "NSView+OptiqueBackground.h"
+#import "NSGraphicsContext+GraphicsContext.h"
 #import "OPTextLayer.h"
 
 @implementation OPPhotoGridItemView
@@ -34,50 +35,65 @@
 {
     NSRect backgroundFrameRect = NSMakeRect(25, 25, 260, 175);
     
-    [NSGraphicsContext saveGraphicsState];
+    [NSGraphicsContext withinGraphicsContext:^{
+        NSShadow *shadow = [[NSShadow alloc] init];
+        shadow.shadowBlurRadius = 3;
+        shadow.shadowColor = [NSColor colorWithCalibratedRed:0.00 green:0.00 blue:0.00 alpha:0.1];
+        shadow.shadowOffset = NSMakeSize(4, -4);
+        [shadow set];
+        
+        [[NSColor whiteColor] set];
+        NSRectFill(backgroundFrameRect);
+        
+        if (self.selected)
+        {
+            NSBezierPath *path = [NSBezierPath bezierPathWithRect:backgroundFrameRect];
+            
+            NSColor *focusRingColor = [NSColor colorWithCalibratedRed:0.22 green:0.46 blue:0.96 alpha:1.00];
+            
+            NSShadow *shadow = [[NSShadow alloc] init];
+            shadow.shadowColor = focusRingColor;
+            shadow.shadowOffset = NSMakeSize(-1, 1);
+            shadow.shadowBlurRadius = 5;
+            [shadow set];
+            [path fill];
+            
+            shadow.shadowOffset = NSMakeSize(1, -1);
+            [shadow set];
+            [path fill];
+        }
+    }];
     
-    NSShadow *shadow = [[NSShadow alloc] init];
-    shadow.shadowBlurRadius = 3;
-    shadow.shadowColor = [NSColor colorWithCalibratedRed:0.00 green:0.00 blue:0.00 alpha:0.1];
-    shadow.shadowOffset = NSMakeSize(4, -4);
-    [shadow set];
     
-    [[NSColor whiteColor] set];
-    NSRectFill(backgroundFrameRect);
+    [NSGraphicsContext withinGraphicsContext:^{
+        NSRect imageRect = NSMakeRect(35, 35, 239, 155);
+        [self.itemImage drawInRect:imageRect fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0 respectFlipped:YES hints:nil];
+    }];
     
-    [NSGraphicsContext restoreGraphicsState];
     
-    [NSGraphicsContext saveGraphicsState];
-    
-    NSRect imageRect = NSMakeRect(35, 35, 239, 155);
-    [self.itemImage drawInRect:imageRect fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0 respectFlipped:YES hints:nil];
-    
-    [NSGraphicsContext restoreGraphicsState];
-    
-    [NSGraphicsContext saveGraphicsState];
-    
-    NSRect textRect = NSMakeRect(backgroundFrameRect.origin.x + 3,
-                                 NSHeight(backgroundFrameRect) + 30,
-                                 NSWidth(backgroundFrameRect) - 6,
-                                 14);
-    
-    OPTextLayer *label = [[OPTextLayer alloc] init];
-    [label setFont:@"Helvetica Neue Regular"];
-    [label setFontSize:12];
-    [label setFrame:textRect];
-    [label setString:self.itemTitle];
-    [label setAlignmentMode:kCAAlignmentCenter];
-    [label setForegroundColor:[[NSColor blackColor] CGColor]];
-    [label setBackgroundColor:[[NSView gradientBottomColor] CGColor]];
-    
-    for (CALayer *layer in self.layer.sublayers)
-    {
-        [layer removeFromSuperlayer];
-    }
-    
-    [self.layer addSublayer:label];
-
-    [NSGraphicsContext restoreGraphicsState];
+    //Draw label
+    [NSGraphicsContext withinGraphicsContext:^{
+        NSRect textRect = NSMakeRect(backgroundFrameRect.origin.x + 3,
+                                     NSHeight(backgroundFrameRect) + 30,
+                                     NSWidth(backgroundFrameRect) - 6,
+                                     14);
+        
+        OPTextLayer *label = [[OPTextLayer alloc] init];
+        [label setFont:@"Helvetica Neue Regular"];
+        [label setFontSize:12];
+        [label setFrame:textRect];
+        [label setString:self.itemTitle];
+        [label setAlignmentMode:kCAAlignmentCenter];
+        [label setForegroundColor:[[NSColor blackColor] CGColor]];
+        [label setBackgroundColor:[[NSView gradientBottomColor] CGColor]];
+        
+        for (CALayer *layer in self.layer.sublayers)
+        {
+            [layer removeFromSuperlayer];
+        }
+        
+        [self.layer addSublayer:label];
+    }];
 }
 
 - (void)viewDidChangeBackingProperties
