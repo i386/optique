11340ef4a7e46adf3f928cfc7f77c6359899f8ea
James Dumay <james.w.dumay@gmail.com>
2013-05-07 07:43:57 +1000

    add stub social plugin for twitter+fb integration

---
+++ Social/OPSocialPlugin.h
@@ -0,0 +1,13 @@
+//
+//  OPSocialPlugin.h
+//  Optique
+//
+//  Created by James Dumay on 7/05/13.
+//  Copyright (c) 2013 James Dumay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OPSocialPlugin : NSObject<XPPlugin>
+
+@end
