6c34049d4ab77c422929b60b4fdf27eba9db7175
James Dumay <james.w.dumay@gmail.com>
2014-02-01 13:04:55 +1100

    Add darkroom bundle

---
+++ Darkroom/OPDarkroomPlugin.m
@@ -0,0 +1,23 @@
+//
+//  OPDarkroomPlugin.m
+//  Optique
+//
+//  Created by James Dumay on 1/02/2014.
+//  Copyright (c) 2014 James Dumay. All rights reserved.
+//
+
+#import "OPDarkroomPlugin.h"
+
+@implementation OPDarkroomPlugin
+
+-(NSToolbarItem*)toolbarItemForIdentifier:(NSString*)identifier
+{
+    return nil;
+}
+
+-(void)addToolbarItemsForToolbar:(NSToolbar*)toolbar
+{
+    
+}
+
+@end
