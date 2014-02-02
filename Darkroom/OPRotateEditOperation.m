1222f54174b5a28be61be9062eaeb574fe4cef13
James Dumay <james.w.dumay@gmail.com>
2014-02-03 00:07:13 +1100

    Rotate image, in all its buggy glory.

--- Darkroom/OPRotateEditOperation.m
+++ Darkroom/OPRotateEditOperation.m
@@ -20,25 +20,78 @@
 
 -(void)performWithItem:(id<XPItem>)item
 {
-    CGImageSourceRef sourceRef = CGImageSourceCreateWithURL((__bridge CFURLRef)(item.url), nil);
+    //Load original with metadata
+    NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:(id)kCGImageSourceShouldCache, kCFBooleanFalse, nil];
+    CGImageSourceRef sourceRef = CGImageSourceCreateWithURL((__bridge CFURLRef)(item.url), (__bridge CFDictionaryRef)(options));
     CFDictionaryRef properties = CGImageSourceCopyPropertiesAtIndex(sourceRef, 0, nil);
-    NSLog(@"Image properties %@", (__bridge NSDictionary*)properties);
-    CFNumberRef orientation = CFDictionaryGetValue(properties, kCGImagePropertyOrientation);
-    
-    NSInteger value;
-    CFNumberGetValue(orientation, kCFNumberNSIntegerType, &value);
-    value++;
-    
     CFMutableDictionaryRef newImageProperties = CFDictionaryCreateMutableCopy(nil, 0, properties);
     
-    orientation = CFNumberCreate(NULL, kCFNumberNSIntegerType, &value);
+    CGImageRef theImage = CGImageSourceCreateImageAtIndex(sourceRef, 0, nil);
+
+    CGFloat pixelsHeight = CGImageGetHeight(theImage);
+    CGFloat pixelsWidth = CGImageGetWidth(theImage);
     
-    CFDictionaryAddValue(newImageProperties, kCGImagePropertyOrientation, (void *)(orientation));
+    //Flip dimensions
+    CGRect dimensions = CGRectMake(0, 0, pixelsWidth, pixelsHeight);
     
+    //
+    CGContextRef context = CreateBitmapContext(theImage, CGSizeMake(pixelsHeight, pixelsWidth));
+    
+    CGAffineTransform  tran = CGAffineTransformIdentity;
+    tran = CGAffineTransformMakeTranslation(0.0, pixelsWidth);
+    tran = CGAffineTransformRotate(tran, -(90 * M_PI / 180));
+    CGContextScaleCTM(context, -1.0, 1.0);
+    CGContextTranslateCTM(context, -pixelsHeight, 0.0);
+    CGContextConcatCTM(context, tran);
+    CGAffineTransform flipVertical = CGAffineTransformMake(1, 0, 0, -1, 0, pixelsHeight);
+    CGContextConcatCTM(context, flipVertical);
+    
+    CGContextDrawImage(context, dimensions, theImage);
+	
+    CGImageRef cgImageRotated = CGBitmapContextCreateImage(context);
+    
+    //Save, with original metadata
     CGImageDestinationRef destinationRef = CGImageDestinationCreateWithURL((__bridge CFURLRef)(item.url), CGImageSourceGetType(sourceRef), 0, nil);
-    CGImageDestinationAddImageFromSource(destinationRef, sourceRef, 0, nil);
-    CGImageDestinationSetProperties(destinationRef, newImageProperties);
+    CGImageDestinationAddImage(destinationRef, cgImageRotated, newImageProperties);
     CGImageDestinationFinalize(destinationRef);
+    
+    //Cleanup
+    CFRelease(destinationRef);
+    CFRelease(sourceRef);
+//    CGImageRelease(theImage);
+//    CGImageRelease(cgImageRotated);
+//    CGContextRelease(cgContextRef);
+}
+
+CGContextRef CreateBitmapContext (CGImageRef inImage, CGSize size)
+{
+    CGFloat bitmapBytesPerRow = (size.width * 4) * size.height;
+    
+    // Preserve color space
+    CGColorSpaceRef colorSpace = CGImageGetColorSpace(inImage);
+    if (colorSpace == NULL)
+    {
+        fprintf(stderr, "Error allocating color space\n");
+        return NULL;
+    }
+    
+    //Create a new context with the same format of the original
+    CGContextRef context = CGBitmapContextCreate (NULL,
+                                     size.width,
+                                     size.height,
+                                     CGImageGetBitsPerComponent(inImage),
+                                     bitmapBytesPerRow,
+                                     colorSpace,
+                                     CGImageGetAlphaInfo(inImage));
+    if (context == NULL)
+    {
+        fprintf (stderr, "Context not created!\n");
+    }
+    
+    // Make sure and release colorspace before returning
+    CGColorSpaceRelease( colorSpace );
+    
+    return context;
 }
 
 @end
